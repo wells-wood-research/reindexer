bdee59b18ac466f27d3175b8ac677b3247a8a30f
Marta Chronowska <mchrnwsk@gmail.com>
2025-04-02 21:44:53 +0100

    Remove isomer check functionality as obsolete and add error for maximum common substructure fail

--- src/utils/errors.py
+++ src/utils/errors.py
@@ -2,6 +2,6 @@ class XYZFileFormatError(Exception):
     """Exception raised for errors in the XYZ file format."""
     pass
 
-class IsomerMismatchError(Exception):
-    """Exception raised when two molecules are not isomers and cannot be matched."""
+class SubstructureNotFound(Exception):
+    """Exception raised when RDKit's findMCS (maximum common substructure) calculation failed to finish."""
     pass
