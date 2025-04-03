f8dc911d4b47416dfccb5e60473fc96279180eb9
Marta Chronowska <mchrnwsk@gmail.com>
2025-04-03 13:54:28 +0100

    Error when structure optimisation can't converge

--- src/utils/errors.py
+++ src/utils/errors.py
@@ -5,3 +5,6 @@ class XYZFileFormatError(Exception):
 class SubstructureNotFound(Exception):
     """Exception raised when RDKit's findMCS (maximum common substructure) calculation failed to finish."""
     pass
+
+class StructureNotOptimised(Exception):
+    """Structure optimisation with rdkit.Chem.rdForceFieldHelpers.UFFOptimizeMolecule did not converge"""
